bc59fc1b44244ec8718ac867e04409efb4846733
ahmedelsayed101 <ahmedessayed101@gmail.com>
2022-05-13 19:41:55 +0200

    Add aws_instance jenkins_server resource

--- launch-aws-ec2-jenkins-instance/main.tf
+++ launch-aws-ec2-jenkins-instance/main.tf
@@ -48,3 +48,35 @@ resource "aws_security_group" "allow_ssh_sg" {
 }
 
 
+# jenkins aws ec2 instance
+resource "aws_instance" "jenkins_server" {
+  ami                    = data.aws_ami.ubuntu_ami.id
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh_sg.id]
+
+
+
+  root_block_device {
+    volume_size = var.volume_size
+  }
+
+  user_data = base64encode(templatefile("${path.module}/jenkins-provision.sh", {
+    aws_access_key = var.aws_access_key
+    aws_secret_key = var.aws_secret_key
+    region         = var.region
+    instance_type  = var.instance_type
+  }))
+
+  connection {
+    host        = coalesce(self.public_ip, self.private_ip)
+    agent       = true
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("${path.module}/key")
+  }
+
+  tags = {
+    Name = "${var.name}"
+  }
+}
