c4302470595f929e0d2d1eb88227f4e049f1bb51
ahmedelsayed101 <ahmedessayed101@gmail.com>
2022-05-13 19:40:37 +0200

    Add aws_security_group

--- launch-aws-ec2-jenkins-instance/main.tf
+++ launch-aws-ec2-jenkins-instance/main.tf
@@ -17,4 +17,34 @@ resource "aws_key_pair" "deployer" {
 }
 
 
+# aws security group to allow ssh connection
+resource "aws_security_group" "allow_ssh_sg" {
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    }
+  ]
+}
+
 
