b2c6e144d484268d1079dca3f6d0b827c018cd1f
ahmedelsayed101 <ahmedessayed101@gmail.com>
2022-05-13 20:43:34 +0200

    Add ingress rule to allow inbound traffic for port 8080

--- launch-aws-ec2-jenkins-instance/main.tf
+++ launch-aws-ec2-jenkins-instance/main.tf
@@ -43,6 +43,17 @@ resource "aws_security_group" "allow_ssh_sg" {
       security_groups  = []
       self             = false
       to_port          = 22
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 8080
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 8080
     }
   ]
 }
